fec341cbc9586ba86f509411d3ac850431372c42
melihsnyldzz-ui <melihsnyldzz@gmail.com>
2026-05-06 15:35:09 +0300

    v1.20.5 Vega stok import önizleme sürüm bilgisini güncelle

--- src/config/appVersion.js
+++ src/config/appVersion.js
@@ -1,2 +1,2 @@
-export const APP_VERSION = "v1.20.4";
-export const APP_STAGE = "Vega read-only ilk test prosedürü ve operatör kontrol rehberi";
+export const APP_VERSION = "v1.20.5";
+export const APP_STAGE = "Vega stok import önizleme altyapısı";
